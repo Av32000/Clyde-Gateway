d8ab96b326665667174f5c72db5618c3d9c565bd
Av32000 <ae.av32000@gmail.com>
2023-06-10 12:27:54 +0200

    Add PresenceUpdate

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const ws = require('ws');
-const { Intents } = require("./src/Intents")
+const ws = require("ws");
+const { Intents } = require("./src/Intents");
+const { ActivityType } = require("./src/ActivityType");
 
 class Client {
-  finalIntents = 0
-  gateway = null
-  hearthbeat = 0
-  properties = {}
-  token = ""
-  callbacks = []
+  finalIntents = 0;
+  gateway = null;
+  hearthbeat = 0;
+  properties = {};
+  token = "";
+  callbacks = [];
 
   constructor(token, intents, properties) {
     if (!Array.isArray(intents)) {
-      throw new Error('Please provide a valid array of intentions');
+      throw new Error("Please provide a valid array of intentions");
     }
 
     for (const intent of intents) {
       this.finalIntents |= intent;
     }
 
-    this.token = token
-    this.properties = properties
-    this.gateway = new ws("wss://gateway.discord.gg/?v=10&encoding=json")
+    this.token = token;
+    this.properties = properties;
+    this.gateway = new ws("wss://gateway.discord.gg/?v=10&encoding=json");
 
-    this.gateway.on("message", (message => {
-      const json = JSON.parse(message.toString())
+    this.gateway.on("message", (message) => {
+      const json = JSON.parse(message.toString());
       if (json.op === 10) {
-        this.hearthbeat = json.d.heartbeat_interval
-        this.StartHearthbeat()
+        this.hearthbeat = json.d.heartbeat_interval;
+        this.StartHearthbeat();
       } else if (json.op === 0) {
-        this.callbacks.forEach(element => {
+        this.callbacks.forEach((element) => {
           if (element.event === json.t) {
-            element.callbacks.forEach(callback => {
-              callback(json.d)
-            })
+            element.callbacks.forEach((callback) => {
+              callback(json.d);
+            });
           }
-        })
+        });
       }
-    }))
+    });
 
-    this.gateway.on("open", () => { this.login() })
+    this.gateway.on("open", () => {
+      this.login();
+    });
   }
 
   login() {
@@ -50,39 +53,54 @@ class Client {
         properties: this.properties || {
           os: "linux",
           browser: "clyde-gateway",
-          device: "clyde-gateway"
-        }
-      }
-    }
-    this.gateway.send(JSON.stringify(payload))
+          device: "clyde-gateway",
+        },
+      },
+    };
+    this.gateway.send(JSON.stringify(payload));
   }
 
   on(event, callback) {
-    let exist = false
-    this.callbacks.forEach(element => {
+    let exist = false;
+    this.callbacks.forEach((element) => {
       if (element.event === event) {
-        element.callbacks.push(callback)
-        exist = true
+        element.callbacks.push(callback);
+        exist = true;
       }
-    })
+    });
 
-    if (!exist) this.callbacks.push({ event: event, callbacks: [callback] })
+    if (!exist) this.callbacks.push({ event: event, callbacks: [callback] });
+  }
+
+  updatePresence(status, activity, afk = true) {
+    this.gateway.send(
+      JSON.stringify({
+        op: 3,
+        d: {
+          since: Date.now(),
+          activities: [activity],
+          status: status,
+          afk: afk,
+        },
+      })
+    );
   }
 
   async StartHearthbeat() {
-    await new Promise(r => setTimeout(r, this.hearthbeat * 0.8));
+    await new Promise((r) => setTimeout(r, this.hearthbeat * 0.8));
     const payload = {
       op: 1,
       d: {
-        heartbeat_interval: this.hearthbeat
-      }
-    }
-    this.gateway.send(JSON.stringify(payload))
-    this.StartHearthbeat()
+        heartbeat_interval: this.hearthbeat,
+      },
+    };
+    this.gateway.send(JSON.stringify(payload));
+    this.StartHearthbeat();
   }
 }
 
 module.exports = {
   Client,
-  Intents
-}
+  Intents,
+  ActivityType
+};
---
+++ src/ActivityType.js
@@ -0,0 +1,12 @@
+const ActivityType = {
+    GAME: 0,
+    STREAMING: 1,
+    LISTENING:2,
+    WATCHING:3,
+    CUSTOM:4,
+    COMPETING:5
+}
+
+module.exports = {
+    ActivityType
+}
